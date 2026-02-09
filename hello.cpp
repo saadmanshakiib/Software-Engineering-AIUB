b95f016918e5e99a763a0a6b980c169f27fa9189
saadmanshakiib <saadman@Sadmans-MacBook-Air.local>
2026-02-09 16:03:31 +0600

    2nd time dilam

---
+++ hello.cpp
@@ -0,0 +1,51 @@
+// saadman@Sadmans-MacBook-Air SW % git status
+// fatal: not a git repository (or any of the parent directories): .git
+// saadman@Sadmans-MacBook-Air SW % git init
+// hint: Using 'master' as the name for the initial branch. This default branch name
+// hint: is subject to change. To configure the initial branch name to use in all
+// hint: of your new repositories, which will suppress this warning, call:
+// hint:
+// hint:   git config --global init.defaultBranch <name>
+// hint:
+// hint: Names commonly chosen instead of 'master' are 'main', 'trunk' and
+// hint: 'development'. The just-created branch can be renamed via this command:
+// hint:
+// hint:   git branch -m <name>
+// hint:
+// hint: Disable this message with "git config set advice.defaultBranchName false"
+// Initialized empty Git repository in /Users/saadman/Desktop/SW/.git/
+// saadman@Sadmans-MacBook-Air SW % git status
+// On branch master
+
+// No commits yet
+
+// nothing to commit (create/copy files and use "git add" to track)
+// saadman@Sadmans-MacBook-Air SW % git config --global user.name "saadmanshakiib"
+// saadman@Sadmans-MacBook-Air SW % git remote add origin https://github.com/saadmanshakiib/Software-Engineering-AIUB.git
+// saadman@Sadmans-MacBook-Air SW % git branch -M main
+// saadman@Sadmans-MacBook-Air SW % git add hello.cpp
+// saadman@Sadmans-MacBook-Air SW % git commit -m "Dilam"
+// [main (root-commit) 2e80948] Dilam
+//  Committer: saadmanshakiib <saadman@Sadmans-MacBook-Air.local>
+// Your name and email address were configured automatically based
+// on your username and hostname. Please check that they are accurate.
+// You can suppress this message by setting them explicitly:
+
+//     git config --global user.name "Your Name"
+//     git config --global user.email you@example.com
+
+// After doing this, you may fix the identity used for this commit with:
+
+//     git commit --amend --reset-author
+
+//  1 file changed, 0 insertions(+), 0 deletions(-)
+//  create mode 100644 hello.cpp
+// saadman@Sadmans-MacBook-Air SW % git push -u origin main
+// Enumerating objects: 3, done.
+// Counting objects: 100% (3/3), done.
+// Writing objects: 100% (3/3), 220 bytes | 220.00 KiB/s, done.
+// Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+// To https://github.com/saadmanshakiib/Software-Engineering-AIUB.git
+//  * [new branch]      main -> main
+// branch 'main' set up to track 'origin/main'.
+// saadman@Sadmans-MacBook-Air SW % 
